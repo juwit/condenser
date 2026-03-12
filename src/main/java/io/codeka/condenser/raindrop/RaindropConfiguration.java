05f922f2b293e54ac021e1545d04d6394224165b
Julien WITTOUCK <julien.wittouck@gmail.com>
2026-03-12 11:47:10 +0100

    ✨ : add raindrop configuration class as a main entrypoint

---
+++ src/main/java/io/codeka/condenser/raindrop/RaindropConfiguration.java
@@ -0,0 +1,9 @@
+package io.codeka.condenser.raindrop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class RaindropConfiguration {
+}
