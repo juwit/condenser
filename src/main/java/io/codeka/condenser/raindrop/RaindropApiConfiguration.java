2cb3b79017399e76772a4fa621c062ff056fff17
Julien WITTOUCK <julien.wittouck@gmail.com>
2026-03-12 11:47:47 +0100

    ✨ : add api key and http client header configuration

--- src/main/java/io/codeka/condenser/raindrop/RaindropApiConfiguration.java
+++ src/main/java/io/codeka/condenser/raindrop/RaindropApiConfiguration.java
@@ -1,9 +1,20 @@
 package io.codeka.condenser.raindrop;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
 import org.springframework.web.service.registry.ImportHttpServices;
 
 @Configuration
 @ImportHttpServices(group = "raindrop", basePackageClasses = RaindropApiConfiguration.class)
 public class RaindropApiConfiguration {
+
+    @Value("${RAINDROP_API_KEY}")
+    private String raindropApiKey;
+
+    @Bean
+    RestClientHttpServiceGroupConfigurer myHttpServiceGroupConfigurer() {
+        return (groups) -> groups.forEachClient((_, clientBuilder) -> clientBuilder.defaultHeader("Authorization", "Bearer " + raindropApiKey));
+    }
 }
